d18bc24f859790c4a66b4c12ec16893d563ff0a6
iammiguelsolis <miguelsoliscunza24@gmail.com>
2025-07-31 16:39:28 -0500

    newversiosaddsadasn

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const serverless = require('serverless-http')
-const logger = require('../loggerMiddleware') // sube un nivel
+const logger = require('../loggerMiddleware')
 
 const app = express()
 
@@ -10,24 +10,9 @@ app.use(express.json())
 app.use(logger)
 
 let notes = [
-  {
-    id: 1,
-    content: 'HTML > JAVA',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'HELLO WORLD',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GG SYDA',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
+  { id: 1, content: 'HTML > JAVA', date: '2019-05-30T17:30:31.098Z', important: true },
+  { id: 2, content: 'HELLO WORLD', date: '2019-05-30T18:39:34.091Z', important: false },
+  { id: 3, content: 'GG SYDA', date: '2019-05-30T19:20:14.298Z', important: true }
 ]
 
 app.get('/', (req, res) => {
@@ -41,8 +26,7 @@ app.get('/api/notes', (req, res) => {
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id)
   const note = notes.find(note => note.id === id)
-  if (note) res.json(note)
-  else res.status(404).end()
+  note ? res.json(note) : res.status(404).end()
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -67,5 +51,4 @@ app.use((req, res) => {
   res.status(404).json({ error: 'unknown endpoint' })
 })
 
-// Exporta como función serverless
-module.exports = serverless(app)
+module.exports.handler = serverless(app)
